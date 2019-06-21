72eb186c430c95a35389aa11ad9fe2bf5b133aaf
Bill <313812485@qq.com>
2019-06-21 15:09:06 +0800

    start

---
+++ Shortcut/Shortcut/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shortcut
+//
+//  Created by Bill on 2019/6/21.
+//  Copyright © 2019年 Bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shortcut/Shortcut/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Shortcut
+//
+//  Created by Bill on 2019/6/21.
+//  Copyright © 2019年 Bill. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
